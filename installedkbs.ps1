8c104ddcbde9c4a0c7e5c216695fbdbf0e3f889d
zparker0110 <33632102+zparker0110@users.noreply.github.com>
2023-04-11 09:23:57 -0400

    Add files via upload

---
+++ installedkbs.ps1
@@ -0,0 +1,32 @@
+# PowerShell script to list all installed security-related KBs on a Windows 10 or 11 machine in a table format, including KB article ID, title, and description
+
+$Session = New-Object -ComObject Microsoft.Update.Session
+$Searcher = $Session.CreateUpdateSearcher()
+$Criteria = "IsInstalled=1 and Type='Software' and CategoryIDs contains 'E6CF1350-C01B-414D-A61F-263D14D133B4'"
+$SearchResult = $Searcher.Search($Criteria).Updates
+
+if($SearchResult.Count -eq 0){
+    Write-Host "No installed security-related KBs found"
+}
+else{
+    $KBUpdates = @()
+
+    foreach($Update in $SearchResult){
+        $Title = $Update.Title
+        $KBArticleIDs = $Update.KBArticleIDs
+        $Description = $Update.Description
+        $SupportUrl = $Update.SupportUrl
+        $Categories = $Update.Categories | Select-Object -ExpandProperty Name
+        $ReleaseDate = $Update.LastDeploymentChangeTime
+        $KBUpdates += New-Object psobject -Property @{
+            KBArticleID = $KBArticleIDs
+            Title = $Title
+            Description = $Description
+            SupportUrl = $SupportUrl
+            Categories = $Categories -join ', '
+            ReleaseDate = $ReleaseDate
+        }
+    }
+
+    $KBUpdates | Sort-Object KBArticleID | Format-Table @{Label="KB Article ID";Expression={$_.KBArticleID};width=20}, @{Label="Title";Expression={$_.Title};width=40}, @{Label="Description";Expression={$_.Description};width=80}, @{Label="Support URL";Expression={$_.SupportUrl};width=60}, @{Label="Categories";Expression={$_.Categories};width=40}, @{Label="Release Date";Expression={$_.ReleaseDate};width=20} -Wrap
+}
