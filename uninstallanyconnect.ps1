0739e206e575e09adec5a5540e0af1d34a300169
worldwidez <33632102+worldwidez@users.noreply.github.com>
2023-09-19 12:34:35 -0400

    Create uninstallanyconnect.ps1

---
+++ uninstallanyconnect.ps1
@@ -0,0 +1,17 @@
+$applicationname = "Cisco AnyConnect"
+$applicationlist = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
+    Get-ItemProperty |
+        Where-Object {$_.DisplayName -match "$applicationname" } | 
+            Select-Object -Property Displayname, UninstallString
+ 
+ForEach ($instance in $applicationlist)
+{
+	If ($instance.UninstallString)
+	{
+		$uninst = $instance.UninstallString
+        $uninst = $uninst.Replace('Program Files (x86)', 'PROGRA~2')
+        $uninst = $uninst.Replace('Cisco AnyConnect Secure Mobility Client', 'CISCOA~2')
+		$uninst = $uninst.Replace('/I', '/X ')
+		& cmd -ArgumentList "/c $uninst /quiet /norestart REBOOT=ReallySuppress"
+	}
+}
